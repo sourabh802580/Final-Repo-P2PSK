b0ec65f4b0af209d505a06a16982dc66306e303d
RohitBagal75 <bagalrohit75@gmail.com>
2025-10-10 14:16:16 +0530

    change by mayur all

--- P2PERP/Controllers/InventoryController.cs
+++ P2PERP/Controllers/InventoryController.cs
@@ -1474,6 +1474,7 @@ namespace P2PERP.Controllers
                         ItemId = itemId,
                         ItemCode = row["ItemCode"]?.ToString(),
                         Description = row["Description"]?.ToString(),
+                        ReorderQuantity = row["ReorderQuantity"]?.ToString(),
                         UOMName = row["UOMName"]?.ToString()
                     };
                 }
